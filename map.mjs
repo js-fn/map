74bc1c48cd0f4e24be3b347398ed96ca7b374e01
Andrea Parodi <andrea@parro.it>
2020-04-05 12:19:57 +0200

    map.async

--- map.mjs
+++ map.mjs
@@ -1,1 +1,54 @@
-export function* map() {}
+function* _map(iterable, fn) {
+  let i = 0;
+  for (const item of iterable) {
+    yield fn(item, i);
+    i++;
+  }
+}
+
+async function* _mapAsync(iterable, fn) {
+  let i = 0;
+  for await (const item of iterable) {
+    yield fn(item, i);
+    i++;
+  }
+}
+
+export function map(iterable, fn = iterable) {
+  // this is a clever trick, but it leave some space to
+  // inconsistencies like map(fn,fn) or map(2,2) that create a partial
+  // application. Previopusly it was:
+  // if (typeof fn === "undefined" && typeof iterable == "function") {
+  //   fn = iterable;
+
+  if (fn === iterable) {
+    // partial application of map
+    return (iterable) => map(iterable, fn);
+  }
+
+  if (isAsyncIterable(iterable)) {
+    return _mapAsync(iterable, fn);
+  }
+
+  if (isIterable(iterable)) {
+    return _map(iterable, fn);
+  }
+
+  throw new Error("Iterable argument expected.");
+}
+
+function isIterable(iter) {
+  return (
+    typeof iter === "object" &&
+    iter !== null &&
+    typeof iter[Symbol.iterator] === "function"
+  );
+}
+
+function isAsyncIterable(iter) {
+  return (
+    typeof iter === "object" &&
+    iter !== null &&
+    typeof iter[Symbol.asyncIterator] === "function"
+  );
+}
--- map_test.mjs
+++ map_test.mjs
@@ -4,3 +4,54 @@ import { map } from "./map.mjs";
 test("map is defined", (t) => {
   t.is(typeof map, "function");
 });
+
+test("map returns an iterable that emit function application results", (t) => {
+  const res = map([1, 2], String);
+  t.deepEqual([...res], ["1", "2"]);
+  t.is(typeof res[Symbol.iterator], "function");
+});
+
+test("map throw if argument is not an iterable", (t) => {
+  t.throws(() => map(42, () => 0).next(), "Iterable argument expected.");
+});
+
+test("map returns a partially applied function when given no iterable", (t) => {
+  const zero = map(() => 0);
+  const zeros = zero([1, 2, 3]);
+  t.deepEqual([...zeros], [0, 0, 0]);
+});
+
+test("map accepts generator functions", (t) => {
+  t.deepEqual([...map(odd(), (n) => n + 1)], [2, 4, 6, 8]);
+});
+
+test("map accepts async generator functions", async (t) => {
+  const res = map(asyncOdd(), (n) => n + 1);
+  const arr = [];
+  for await (const it of res) {
+    arr.push(it);
+  }
+  t.deepEqual(arr, [2, 4, 6, 8]);
+});
+
+function* odd() {
+  yield 1;
+  yield 3;
+  yield 5;
+  yield 7;
+}
+
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function* asyncOdd() {
+  yield 1;
+  await wait(10);
+  yield 3;
+  await wait(30);
+  yield 5;
+  await wait(50);
+  yield 7;
+  await wait(70);
+}
