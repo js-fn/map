912a00910bc656ace9cf139a3ba8d6ad960492e7
Andrea Parodi <andrea@parro.it>
2020-04-05 14:03:38 +0200

    options

--- map.mjs
+++ map.mjs
@@ -1,27 +1,42 @@
-export function map(iterable, fn = iterable) {
+export function map(iterable, options = iterable) {
   // this is a clever trick, but it leave some space to
   // inconsistencies like map(fn,fn) or map(2,2) that create a partial
   // application. Previopusly it was:
   // if (typeof fn === "undefined" && typeof iterable == "function") {
   //   fn = iterable;
 
-  if (fn === iterable) {
+  if (options === iterable) {
     // partial application of map
-    return (iterable) => map(iterable, fn);
+    return (iterable) => map(iterable, options);
   }
 
-  if (isAsyncIterable(iterable)) {
-    return _mapAsync(iterable, fn);
+  const opts = _mkOptions(options);
+
+  if (isAsyncIterable(iterable) || opts.forceAsync) {
+    return _mapAsync(iterable, opts);
   }
 
   if (isIterable(iterable)) {
-    return _map(iterable, fn);
+    return _map(iterable, opts);
   }
 
   throw new Error("Iterable argument expected.");
 }
 
-function* _map(iterable, fn) {
+const _defaultOptions = {
+  fn: (a) => a,
+  forceAsync: false,
+};
+
+function _mkOptions(options) {
+  if (typeof options === "function") {
+    options = { fn: options };
+  }
+
+  return Object.assign({}, _defaultOptions, options);
+}
+
+function* _map(iterable, { fn }) {
   let i = 0;
   for (const item of iterable) {
     yield fn(item, i);
@@ -29,7 +44,7 @@ function* _map(iterable, fn) {
   }
 }
 
-async function* _mapAsync(iterable, fn) {
+async function* _mapAsync(iterable, { fn }) {
   let i = 0;
   for await (const item of iterable) {
     yield fn(item, i);
--- map_test.mjs
+++ map_test.mjs
@@ -21,17 +21,40 @@ test("map returns a partially applied function when given no iterable", (t) => {
   t.deepEqual([...zeros], [0, 0, 0]);
 });
 
+const inc = (n) => n + 1;
+
 test("map accepts generator functions", (t) => {
-  t.deepEqual([...map(odd(), (n) => n + 1)], [2, 4, 6, 8]);
+  t.deepEqual([...map(odd(), inc)], [2, 4, 6, 8]);
+});
+
+test("map default function is identity", (t) => {
+  t.deepEqual([...map(odd(), {})], [1, 3, 5, 7]);
+});
+
+test("map can transform a sync iterator of promises into async iterator", async (t) => {
+  const asyncIterator = map(oddPromises(), { forceAsync: true });
+  t.false(isIterable(asyncIterator));
+  t.true(isAsyncIterable(asyncIterator));
+  t.deepEqual(await awaitIntoArray(asyncIterator), [1, 3, 5, 7]);
+});
+
+test("map await fn results if promises", async (t) => {
+  const asyncIterator = map(oddPromises(), {
+    forceAsync: true,
+    fn: (a) => Promise.resolve(a * 2),
+  });
+  t.false(isIterable(asyncIterator));
+  t.true(isAsyncIterable(asyncIterator));
+  t.deepEqual(await awaitIntoArray(asyncIterator), [2, 6, 10, 14]);
+});
+
+test("map accepts options object", (t) => {
+  t.deepEqual([...map(odd(), { fn: inc })], [2, 4, 6, 8]);
 });
 
 test("map accepts async generator functions", async (t) => {
-  const res = map(asyncOdd(), (n) => n + 1);
-  const arr = [];
-  for await (const it of res) {
-    arr.push(it);
-  }
-  t.deepEqual(arr, [2, 4, 6, 8]);
+  const res = map(asyncOdd(), inc);
+  t.deepEqual(await awaitIntoArray(res), [2, 4, 6, 8]);
 });
 
 function* odd() {
@@ -41,17 +64,48 @@ function* odd() {
   yield 7;
 }
 
+function* oddPromises() {
+  yield Promise.resolve(1);
+  yield Promise.resolve(3);
+  yield Promise.resolve(5);
+  yield Promise.resolve(7);
+}
+
 function wait(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function awaitIntoArray(asyncIterable) {
+  const arr = [];
+  for await (const it of asyncIterable) {
+    arr.push(it);
+  }
+  return arr;
+}
+
+function isIterable(iter) {
+  return (
+    typeof iter === "object" &&
+    iter !== null &&
+    typeof iter[Symbol.iterator] === "function"
+  );
+}
+
+function isAsyncIterable(iter) {
+  return (
+    typeof iter === "object" &&
+    iter !== null &&
+    typeof iter[Symbol.asyncIterator] === "function"
+  );
 }
 
 async function* asyncOdd() {
   yield 1;
   await wait(10);
   yield 3;
-  await wait(30);
+  await wait(60);
   yield 5;
   await wait(50);
   yield Promise.resolve(7);
-  await wait(70);
+  await wait(20);
 }
